25e2554f452cc485d7cb86db2a2e79537b0674d5
Vu Anh Quang <quang.vu1998@gmail.com>
2021-06-01 10:12:28 +0700

    write the first program

--- basics.py
+++ basics.py
@@ -1,3 +1,20 @@
-from time import*
-sleep(3)
-print("hello")
+
+def sentence(phrase):
+    interrogatives=("how","when",'what')
+    capitalPhrase=phrase.capitalize()
+    if phrase.startswith(interrogatives):
+        return f'{capitalPhrase}?'
+    else:
+        return f'{capitalPhrase}.'
+
+finalResult=[]
+while True:
+    user_input=input("Say somthing: ")
+    if user_input== "\end":
+        break
+    else :
+        finalResult.append(sentence(user_input))
+
+print(" ".join(finalResult))
+
+
