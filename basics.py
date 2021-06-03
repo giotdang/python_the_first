79d031ba8389bb7298d12dc7ed0285d215471143
Vu Anh Quang <quang.vu1998@gmail.com>
2021-06-03 16:48:48 +0700

    code ling ting

--- basics.py
+++ basics.py
@@ -8,13 +8,29 @@ def sentence(phrase):
         return f'{capitalPhrase}.'
 
 finalResult=[]
-while True:
-    user_input=input("Say somthing: ")
-    if user_input== "\end":
-        break
-    else :
-        finalResult.append(sentence(user_input))
+#while True:
+#    user_input=input("Say somthing: ")
+#    if user_input== "\end":
+#        break
+#    else :
+#        finalResult.append(sentence(user_input))
 
 print(" ".join(finalResult))
+txt = "5-4|2-3|10|4-5"
+
+x = txt.split("|")
+i=0
+for i in range(len(x)):
+    x[i]=x[i].split("-")
+    print(x[i])
+
+rollList=[]
+for item in x:
+    rollList.extend(item)
+
+rollList=[int(i) for i in rollList]
+
+print(x)
+print(rollList)
 
 
